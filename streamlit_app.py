453720cdb8eec6405c51507da3cd22cef48bf59c
Shanmukh_Pendyala <133161765+RocketNiddle@users.noreply.github.com>
2025-04-13 23:40:37 -0500

    Update streamlit_app.py
    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,9 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered,' + name_on_order + '!', icon="✅")
         
+
+
+# New sertion to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
